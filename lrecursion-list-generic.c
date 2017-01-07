36ed5f8da8df711e9412a7042559a7f5f95f5c54
lionswrath <caio.tonetti@gmail.com>
2017-01-07 17:01:03 -0200

    Toma ae drico

--- lrecursion-list-generic.c
+++ lrecursion-list-generic.c
@@ -96,10 +96,22 @@ bool exists(Queue *q, void *d) {
     return false;
 }
 
+// Testing
+Element * getElement(Queue *q, void *d) {
+    if (isQueueEmpty(q)) return NULL;
+    
+    Element *element = q->begin;
+    while (element != NULL) {
+        if (element->data == d) return element;
+        element = element->next;
+    }
+    return NULL;
+}
+
 void push(Queue *q, void *d) {
     Element *element = createElement(d);
 
-    if (q->begin == NULL) {
+    if (isQueueEmpty(q)) {
         q->begin = element;
         q->end = element;
         q->size++;
@@ -109,6 +121,22 @@ void push(Queue *q, void *d) {
     q->end->next = element;
     q->end = element;
     q->size++;
+}
+
+// Testing
+bool pushToPosition(Queue *q, void *d, void *pos) {
+    Element *e = createElement(d);
+    Element *t = getElement(q, pos);
+
+    if (t == NULL) return false;
+    if (t->next == NULL) {
+        t->next = e;
+        e->next = NULL;
+    } else {
+        e->next = t->next;
+        t->next = e;
+    }
+    return true;
 }
 
 // Dá free no element, não no dado
@@ -142,14 +170,45 @@ bool erase(Queue *q, void *d) {
         }
     }
     return false;
-} 
+}
 
+// Dont free the src Queue
+void append(Queue *dst, Queue *src) { 
+    Element *e = src->begin;
+    while (e != NULL) {
+        push(dst, e->data); 
+        e = e->next; 
+    }
+}
+
+// Dont free the src Queue
+bool appendToPosition(Queue *dst, Queue *src, void *pos) {
+    Element *e = src->begin;
+    while (e != NULL) {
+        if (!pushToPosition(dst, e->data, pos)) return false; 
+        pos = e->data; // To put in the correct order
+        e = e->next; 
+    }
+}
+
+// Dont free the data, just the elements
+// High chance of memory leak
+void clearQueue(Queue *q) {
+    Element *e = q->begin;
+    Element *next;
+    while (e != NULL) {
+        next = e->next;
+        free(e);
+        e = next;
+    }
+
+    q->begin = NULL;
+    q->end = NULL;
+}
 //-----------------------------------------------------------SOME VARIABLES
 
 Queue *g;
-Queue *order; // Use the queue itself as the order, null on prod
-
-//-------------------------------------------------------------------MEMORY
+Queue *order; 
 
 //-------------------------------------------------------------------READ
 
@@ -257,24 +316,88 @@ void orderNT(Queue *q, Queue *orderSet) {
 
 //-----------------------------------------------------------------ELIMINATION
 
-Queue * findAllMatches(Queue *grammar) {
+Queue * findAllMatches(Queue *grammar, Number *nl, Number *nr) {
+    Queue *matches = initializeQueue();
+
+    fprintf(stderr, "Ai = %c, Aj = %c\n", nl->value, nr->value);
+    Element *e = grammar->begin;
+    while (e != NULL) {
+        Rule *r = (struct Rule*) e->data;     
+        if (r->l == nl->value) 
+           if (r->prod[0] == nr->value)
+               push(matches, r);
+        e = e->next;
+    }
+
+    return matches;
+}
+
+void imediateElimination(Queue *grammar) {
 
 }
 
-void imediateElimination() {
+void substituteMatches(Queue *grammar, Queue *matches, Number *n) {
+    char *newProduction;     
+    Queue *newRules = initializeQueue();
 
+    // Para cada match
+    Element *e = matches->begin;
+    while (e != NULL) {
+        Rule *m = e->data;
+
+        // Achar todas as produções de n na gramática
+        Element *t = grammar->begin;
+        while (t != NULL) {
+            Rule *r = t->data;
+            if (r->l == n->value) {
+                newProduction = malloc(NCHAR * sizeof(char));
+                newProduction[0] = '\0';
+
+                strcpy(newProduction, r->prod);
+                strcat(newProduction, m->prod + 1);
+
+                fprintf(stdout, "           Prod: %s\n", newProduction);
+
+                // Adicionar nova regra na gramatica
+                push(newRules, createRule(m->l, newProduction));
+            }
+
+            t = t->next;
+        }
+
+        // Unite the newRules to the grammar
+        appendToPosition(grammar, newRules, m);
+        clearQueue(newRules); // Clear the newRules
+        erase(grammar, m);  // Eliminar match da gramática
+        // Free the matched rule - memory leak otherwise
+        e = e->next; 
+    }
+
+    free(newRules);
+    // Free the Queue
 }
 
 void globalElimination(Queue *grammar, Queue *order) {
-    int i, j;
     Element *e = order->begin;
 
-    for (i=0; i<order->size; i++) {
-        for (j=0; j<i-1; j++) {
+    while (e != NULL) {
+        Element *t = order->begin;
+        while (t != e) {
             // Find all Matches
-            // Substituir matches por todas as produções de Aj
-            // Remover todas as recursões imediatas
-            imediateElimination();   
+            // Change to an external match initialization
+            // But internal clearing - memory leak on multiple Queue creations
+            Queue *matches = findAllMatches(grammar, e->data, t->data);
+            
+            if (!isQueueEmpty(matches)) { 
+                printGrammar(stdout, matches);
+                // Substituir matches por todas as produções de Aj
+                substituteMatches(grammar, matches, t->data);
+            }
+
+            // Imediate left recursion elimination
+            imediateElimination(grammar);   
+            // Free the Queue "matches" structure
+            t = t->next;
         }
         e = e->next;   
     }
@@ -313,6 +436,9 @@ int main(int argc, char *argv[]) {
     order = initializeQueue();
     orderNT(g, order);
     printOrder(out, order);
-    
+
+    globalElimination(g, order);
+    printGrammar(out, g);
+
     return 0;
 }
